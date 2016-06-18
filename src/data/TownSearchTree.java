23573819b9b643a2e3b135caf6e86363c22cca7d
agines <agines.dev@gmx.at>
2016-06-18 11:07:58 +0200

    fix

--- src/data/TownSearchTree.java
+++ src/data/TownSearchTree.java
@@ -26,36 +26,28 @@ public class TownSearchTree extends BaseTree<Town> {
 		return find(root, townName);
 	}
 
-	
 	public Node<Town> find(Node<Town> current, String townName) {
-		// TODO Auto-generated method stub
 		if (current == null) {
 			return null;
 		}
 		int vgl = compare(townName, current.getValue());
-		if (vgl == 0) {		// Gefunden
+		if (vgl == 0) { // Gefunden
 			return current;
-		}
-		else if (vgl < 0) {	// Links
+		} else if (vgl < 0) { // Links
 			return find(current.getLeft(), townName);
-		}
-		else {				// Rechts
+		} else { // Rechts
 			return find(current.getRight(), townName);
 		}
-		//return super.find(current, needle);
-	
-	
-	
 	}
-	public int compare(String a, Town b)
-	{
+
+	public int compare(String a, Town b) {
 		if (a == null)
 			return -1;
 		if (b == null)
 			return 1;
 		if (a.equalsIgnoreCase(b.getName()))
 			return 0;
-		return a.compareToIgnoreCase(b.getName());
+		return a.toLowerCase().compareToIgnoreCase(b.getName().toLowerCase());
 	}
-	
+
 }
